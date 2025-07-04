1cc64a6bfd815ed1589f2d845299fca4cc9f0910
EuropGreenSolar <europgreensolar@gmail.com>
2025-07-04 10:25:59 +0200

    appliquer swagger  dans program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -45,11 +45,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Middleware
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 //app.UseHttpsRedirection();
 app.UseCors(MyAllowSpecificOrigins);
